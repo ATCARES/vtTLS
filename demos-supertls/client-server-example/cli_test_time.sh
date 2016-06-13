80548aadc0e47c797e3a826f2a392dfb5cf76765
AndreJoaquim <andrej_21@hotmail.com>
2016-06-13 13:21:00 +0100

    Changed calls in demos after refactor

--- demos-supertls/client-server-example/cli_test_time.sh
+++ demos-supertls/client-server-example/cli_test_time.sh
@@ -1,6 +1,6 @@
 for i in {0..50}
 do
   echo 'Test' +  $i + ' of 50'
-  ./client 172.17.39.6 test10m.txt >> result.txt;
-  sleep 2;
+  ./client 172.17.39.2 100MB.zip >> 100MB_result.txt;
+  sleep 1;
 done
