c102fb776992fc9520c08a3bf9b5d7b6f48af97c
Miguel Pardal <miguel.pardal@tecnico.ulisboa.pt>
2017-11-04 00:17:39 +0100

    Remove commented code; handle file not found and empty file

--- demos-vttls/client-server-example/client.cpp
+++ demos-vttls/client-server-example/client.cpp
@@ -26,243 +26,223 @@
 #define MAX_MSG_SIZE 16250
 #define DIVERSITY 2
 
+// Error checking
 #define CHK_NULL(x) if ((x)==NULL) exit (1)
 #define CHK_ERR(err,s) if ((err)==-1) { perror(s); exit(1); }
 #define CHK_SSL(err) if ((err)==-1) { ERR_print_errors_fp(stderr); exit(2); }
 
+// Buffer settings
+#define BUF_SZ 1024
 
-int main (int argc, char* argv[])
-{
-  int       err;
-  int       sd;
-  struct    sockaddr_in sa;
-  SSL_CTX*  ctx;
-  SSL*      ssl;
-  X509*     server_cert;
-  X509*		server_sec_cert;
-  char*     str;
-  char      buf [4096];
-  SSL_METHOD const *meth;
-  timeval start, end;
+int main(int argc, char* argv[]) {
+	int err;
+	int sd;
+	struct sockaddr_in sa;
+	SSL_CTX* ctx;
+	SSL* ssl;
+	X509* server_cert;
+	X509* server_sec_cert;
+	char* str;
+	char buf[4096];
+	SSL_METHOD const *meth;
+	timeval start, end;
 
-  const char *ip;
-  unsigned int port;
-  const char *file_to_download;
-  const char *file_to_save;
+	/* for performance measurements */
+	unsigned long long diff;
 
-  if(argc != 5){
-    printf("Usage: ./client <server-ip> <server-port> <file-to-download> <file-to-save>\n");
-    exit(0);
-  }
-  ip = argv[1];
-  port = atoi(argv[2]);
-  file_to_download = argv[3];
-  file_to_save = argv[4];
-  debug_printf("Arguments: IP %s Port %u File to download '%s' File to save '%s'\n",
-		  ip, port, file_to_download, file_to_save);
-  
-  SSL_load_error_strings();
-  OpenSSL_add_ssl_algorithms(); /* SSL_library_init() */
-  meth = TLSv1_2_client_method();
-  
-  ctx = SSL_CTX_new (meth);
-  
-  if (!ctx) {
-    ERR_print_errors_fp(stderr);
-    exit(2);
-  }
+	/* arguments */
+	const char *ip;
+	unsigned int port;
+	const char *file_to_download;
+	const char *file_to_save;
 
-  /* ----------------------------------------------- */
-  /* Create a socket and connect to server using normal socket calls. */
-  
-  sd = socket (AF_INET, SOCK_STREAM, 0);
-  CHK_ERR(sd, "socket");
- 
-  memset(&sa, 0, sizeof(sa));
-  
-  sa.sin_family      = AF_INET;
-  sa.sin_addr.s_addr = inet_addr(ip);	/* Server IP */
-  sa.sin_port        = htons(port);		/* Server Port number */
-  
-  err = connect(sd, (struct sockaddr*) &sa, sizeof(sa));
-  CHK_ERR(err, "connect");
+	/* Arguments validation */
+	if (argc != 5) {
+		printf(
+				"Usage: ./client <server-ip> <server-port> <file-to-download> <file-to-save>\n");
+		exit(0);
+	}
+	ip = argv[1];
+	port = atoi(argv[2]);
+	file_to_download = argv[3];
+	file_to_save = argv[4];
+	debug_printf(
+			"Arguments: IP %s Port %u File to download '%s' File to save '%s'\n",
+			ip, port, file_to_download, file_to_save);
 
+	/* SSL initialization */
 
-  /* ----------------------------------------------- */
-  /* Now we have TCP connection. Start SSL negotiation. */
-  
-  ssl = SSL_new (ctx);
-  CHK_NULL(ssl);
-  
-  SSL_set_fd (ssl, sd);
-  /* Sets the file descriptor fd as the input/output
-   * facility for the TLS encypted side
-   * of argument "ssl"; fd is usually the socket descriptor */
-  
-  unsigned long long diff;
-  int i = 0;
-  
-  gettimeofday(&start, NULL);
-  
-  err = SSL_connect (ssl);
-  CHK_SSL(err);
-  
-  gettimeofday(&end, NULL);
-  diff = 1000 * (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) / 1000;
-  debug_printf("The vtTLS Handshake took %llu ms\n", diff);
-  diff = 0;
-  
-  /* ssl->method->ssl_connect(s)*/
-    
-  /* Following two steps are optional and not required for
-     data exchange to be successful. */
-  
-  /* Get the cipher - opt */
+	SSL_load_error_strings();
+	/* SSL_library_init() */
+	OpenSSL_add_ssl_algorithms();
+	meth = TLSv1_2_client_method();
 
-  debug_printf("SSL connection using %s\n", SSL_get_cipher (ssl));
-  debug_printf("SSL connection using %s\n", SSL_get_n_cipher (DIVERSITY, ssl));
-  
+	ctx = SSL_CTX_new(meth);
 
-  /* Get server's certificate (note: beware of dynamic allocation) - opt */
-  /*
-  server_cert = SSL_get_peer_certificate (ssl);
-  CHK_NULL(server_cert);
-  
-  server_sec_cert = SSL_get_second_peer_certificate (ssl);
-  CHK_NULL(server_sec_cert);
-  
-  printf ("Server certificate:\n");
-  
-  str = X509_NAME_oneline (X509_get_subject_name (server_cert),0,0);
-  CHK_NULL(str);
-  printf ("\t subject: %s\n", str);
-  OPENSSL_free (str);
+	if (!ctx) {
+		ERR_print_errors_fp(stderr);
+		exit(2);
+	}
 
-  str = X509_NAME_oneline (X509_get_issuer_name  (server_cert),0,0);
-  CHK_NULL(str);
-  printf ("\t issuer: %s\n", str);
-  OPENSSL_free (str);
+	/* ----------------------------------------------- */
+	/* Create a socket and connect to server using normal socket calls. */
 
-  printf ("Server second certificate:\n");
+	sd = socket(AF_INET, SOCK_STREAM, 0);
+	CHK_ERR(sd, "socket");
 
-  str = X509_NAME_oneline (X509_get_subject_name (server_sec_cert),0,0);
-  CHK_NULL(str);
-  printf ("\t subject: %s\n", str);
-  OPENSSL_free (str);
+	memset(&sa, 0, sizeof(sa));
 
-  str = X509_NAME_oneline (X509_get_issuer_name  (server_sec_cert),0,0);
-  CHK_NULL(str);
-  printf ("\t issuer: %s\n", str);
-  OPENSSL_free (str);
-  */
-  /* We could do all sorts of certificate verification stuff here before
-     deallocating the certificate. */
-/*
-  X509_free (server_cert);
-  X509_free (server_sec_cert);
-  */
+	sa.sin_family = AF_INET;
+	sa.sin_addr.s_addr = inet_addr(ip); /* Server IP */
+	sa.sin_port = htons(port); /* Server Port number */
 
-  /* --------------------------------------------------- */
-  /* DATA EXCHANGE - Send a message and receive a reply. */
+	err = connect(sd, (struct sockaddr*) &sa, sizeof(sa));
+	CHK_ERR(err, "connect");
+	debug_println("Connected to server");
 
-  // send file name to request file
-  err = SSL_write(ssl, file_to_download, strlen(file_to_download));
-  CHK_SSL(err);
-  // add end-of-line
-  err = SSL_write(ssl, "\n", sizeof(char));
-  CHK_SSL(err);
+	/* ----------------------------------------------- */
+	/* Now we have TCP connection. Start SSL negotiation. */
 
-  // send empty line to terminate request
-  err = SSL_write(ssl, "\n", sizeof(char));
-  CHK_SSL(err);
-  
+	ssl = SSL_new(ctx);
+	CHK_NULL(ssl);
 
-  // open file to save
-//  FILE *file_rcv = fopen(file_to_save, "ab+");
-//  file_rcv = fopen(file_to_save, "w+");
-  FILE *file_rcv = fopen(file_to_save, "wb+");
-   
-  // read file size (as a string)
-//  err = readSSLLine(ssl, buf, sizeof(buf) - 1);
-  err = readSSLLine(ssl, buf, sizeof(buf));
-  CHK_SSL(err);
-  int bytesToDownload = atoi(buf);
-  debug_printf("%d bytes to download\n", bytesToDownload);
+	SSL_set_fd(ssl, sd);
+	/* Sets the file descriptor fd as the input/output
+	 * facility for the TLS encrypted side
+	 * of argument "ssl"; fd is usually the socket descriptor */
 
-  // read empty line separator
-  err = readSSLLine(ssl, buf, sizeof(buf));
-  CHK_SSL(err);
-  debug_print("Read separator line\n");
-  
-  int bytesDownloaded = 0;
-  int workBufferSize = 1024;
-  char workBuffer[1024];
-  
-  // read until all expected bytes are received
-  while(bytesDownloaded < bytesToDownload) {
-      int result = SSL_read(ssl, workBuffer, workBufferSize);
-      debug_printf("Read more %d bytes\n", result);
-      if(result == -1) {
-          debug_println("Error while reading data from socket!");
-          return -1;
-      }
-      bytesDownloaded += result;
+	gettimeofday(&start, NULL);
 
-      size_t wrote = fwrite(workBuffer, sizeof(char), result, file_rcv);
-      debug_printf("Wrote %lu bytes to file\n", wrote);
-  }
-  debug_println("Read all expected bytes");
-  
-  
-//  long file_len = strtol(buf, (char**) NULL, 10);
-//  debug_printf("filelen = %ld\n", file_len);
-//
-//  // Enough memory for file + \0
-//  char *buffer = (char *)malloc((file_len+1)*sizeof(char));
-//
-//  int counter = 0;
-//
-//  for (counter = 0; counter < 50; counter++) {
-//  /*************/
-//
-//    err = 0;
-//    int total_size = 0;
-//    i = file_len;
-//
-//    gettimeofday(&start, NULL);
-//
-//    for(i = file_len; i - MAX_MSG_SIZE > 0; i -= MAX_MSG_SIZE){
-//        err += SSL_read (ssl, buffer+err, MAX_MSG_SIZE);
-//    }
-//
-//    err += SSL_read (ssl, buffer+err, i);
-//
-//    gettimeofday(&end, NULL);
-//    diff = 1000 * (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) / 1000;
-//    printf ("%llu\n", diff);
-//    debug_printf("The vtTLS took %llu ms to read %s.\n", diff, file_to_download);
-//    diff = 0;
-//
-//  /*************/
-//  }
-//
-//  fprintf(file_rcv, "%s", buffer);
-//
-//  debug_printf("-- total_size: %d\n", err);
-  
-  /* send SSL/TLS close_notify */
-  SSL_shutdown (ssl);
+	err = SSL_connect(ssl);
+	CHK_SSL(err);
 
+	gettimeofday(&end, NULL);
+	diff = 1000 * (end.tv_sec - start.tv_sec)
+			+ (end.tv_usec - start.tv_usec) / 1000;
+	debug_printf("The vtTLS Handshake took %llu ms\n", diff);
 
-  /* Clean up. */
-  //free(buffer);
-  fclose(file_rcv);
-  
-  close (sd);
-  SSL_free (ssl);
-  SSL_CTX_free (ctx);
-  
-  return 0;
-  
+	/* ssl->method->ssl_connect(s)*/
+
+	/* Following two steps are optional and not required for
+	 data exchange to be successful. */
+
+	/* Get the cipher - opt */
+
+	debug_printf("SSL connection using %s\n", SSL_get_cipher (ssl));
+	debug_printf("SSL connection using %s\n",
+			SSL_get_n_cipher (DIVERSITY, ssl));
+
+	/* Get server's certificate (note: beware of dynamic allocation) - opt */
+
+	server_cert = SSL_get_peer_certificate(ssl);
+	CHK_NULL(server_cert);
+
+	server_sec_cert = SSL_get_n_peer_certificate(DIVERSITY, ssl);
+	CHK_NULL(server_sec_cert);
+
+	debug_println("Server certificate:");
+
+	str = X509_NAME_oneline(X509_get_subject_name(server_cert), 0, 0);
+	CHK_NULL(str);
+	debug_printf("\t subject: %s\n", str);
+	OPENSSL_free(str);
+
+	str = X509_NAME_oneline(X509_get_issuer_name(server_cert), 0, 0);
+	CHK_NULL(str);
+	debug_printf("\t issuer: %s\n", str);
+	OPENSSL_free(str);
+
+	debug_println("Server second certificate:");
+
+	str = X509_NAME_oneline(X509_get_subject_name(server_sec_cert), 0, 0);
+	CHK_NULL(str);
+	debug_printf("\t subject: %s\n", str);
+	OPENSSL_free(str);
+
+	str = X509_NAME_oneline(X509_get_issuer_name(server_sec_cert), 0, 0);
+	CHK_NULL(str);
+	debug_printf("\t issuer: %s\n", str);
+	OPENSSL_free(str);
+
+	/* We could do all sorts of certificate verification stuff here before
+	 deallocating the certificate. */
+
+	X509_free(server_cert);
+	X509_free(server_sec_cert);
+
+	/* --------------------------------------------------- */
+	/* DATA EXCHANGE - Send a message and receive a reply. */
+
+	/* REQUEST */
+
+	// send file name to server to request it
+	err = SSL_write(ssl, file_to_download, strlen(file_to_download));
+	CHK_SSL(err);
+	// add end-of-line
+	err = SSL_write(ssl, "\n", sizeof(char));
+	CHK_SSL(err);
+
+	// send empty line separator
+	err = SSL_write(ssl, "\n", sizeof(char));
+	CHK_SSL(err);
+
+	/* RESPONSE */
+
+	// read file size (as a string)
+	err = readSSLLine(ssl, buf, BUF_SZ);
+	CHK_SSL(err);
+	const int bytesToDownload = atoi(buf);
+	if (bytesToDownload < 0) {
+		printf("File not found on server!\n");
+	} else {
+		debug_printf("%d bytes to download\n", bytesToDownload);
+	}
+
+	// read empty line separator
+	err = readSSLLine(ssl, buf, BUF_SZ);
+	CHK_SSL(err);
+
+	if (bytesToDownload >= 0) {
+		// open file to save
+		FILE *file_rcv = fopen(file_to_save, "wb+");
+
+		if (bytesToDownload > 0) {
+			// read until all expected bytes are received
+			int bytesDownloaded = 0;
+			gettimeofday(&start, NULL);
+			while (bytesDownloaded < bytesToDownload) {
+				int readResult = SSL_read(ssl, buf, BUF_SZ);
+				CHK_SSL(readResult);
+				debug_printf("Read %d bytes from secure socket\n", readResult);
+				bytesDownloaded += readResult;
+				// write bytes to open file
+				size_t writeResult = fwrite(buf, sizeof(char), readResult,
+						file_rcv);
+				CHK_ERR(writeResult, "file");
+				debug_printf("Wrote %lu bytes to file\n", writeResult);
+			}
+			gettimeofday(&end, NULL);
+			diff = 1000 * (end.tv_sec - start.tv_sec)
+					+ (end.tv_usec - start.tv_usec) / 1000;
+			debug_printf("vtTLS took %llu ms to transfer and save %d bytes.\n",
+					diff, bytesDownloaded);
+			debug_println("Read all expected bytes");
+		}
+		// close file
+		fclose(file_rcv);
+		debug_println("File closed");
+	}
+
+	/* send SSL/TLS close_notify */
+	SSL_shutdown(ssl);
+
+	/* Clean up. */
+	close(sd);
+	debug_println("Socket closed");
+
+	SSL_free(ssl);
+	SSL_CTX_free(ctx);
+
+	return 0;
 }
 /* EOF - client.cpp */
--- demos-vttls/client-server-example/server.cpp
+++ demos-vttls/client-server-example/server.cpp
@@ -24,7 +24,6 @@
 #include "debug.h"
 #include "demo.h"
 
-
 /* define HOME to be dir for key and cert files... */
 #define HOME "./"
 /* Make these what you want for cert & key files */
@@ -48,263 +47,239 @@
 #define CHK_ERR(err,s) if ((err)==-1) { perror(s); exit(1); }
 #define CHK_SSL(err) if ((err)==-1) { ERR_print_errors_fp(stderr); exit(2); }
 
+// Buffer settings
+#define BUF_SZ 2048
 
-int main (int argc, char* argv[])
-{
-  int       err;
-  int       listen_sd;
-  int       sd;
-  struct sockaddr_in sa_serv;
-  struct sockaddr_in sa_cli;
-  socklen_t    client_len;
-  SSL_CTX*  ctx;
-  SSL*      ssl;
-  X509*     client_cert;
-  char*     str;
-  char      buf [4096];
-  SSL_METHOD const *meth;
-  unsigned long long diff;
+#define MAX_FILE_NAME 255
+#define MAX_FILE_PATH 4096
 
-  unsigned int port;
+// 2^32=2147483647 plus negative plus thousands separators plus null character
+#define MAX_INT_STRING 10+1+3+1
 
-  if(argc != 2){
-    printf("Usage: ./server <port>\n");
-    exit(1);
-  }  
-  port = atoi(argv[1]);
-  debug_printf("Arguments: Port %d\n", port);
+int main(int argc, char* argv[]) {
+	int err;
+	int listen_sd;
+	int sd;
+	struct sockaddr_in sa_serv;
+	struct sockaddr_in sa_cli;
+	socklen_t client_len;
+	SSL_CTX* ctx;
+	SSL* ssl;
+	X509* client_cert;
+	char* str;
+	char buf[BUF_SZ];
+	SSL_METHOD const *meth;
 
-  demo_banner();
-  
-  /* SSL preliminaries. We keep the certificate and key with the context. */
+	/* buffer for IP address strings */
+	char addr_buf[INET_ADDRSTRLEN];
+	/* buffer for file name */
+	char file_name[MAX_FILE_NAME];
+	/* buffer for file size as string */
+	char file_len_str[MAX_INT_STRING];
 
-  SSL_load_error_strings();
-  OpenSSL_add_ssl_algorithms();
-  meth = TLSv1_2_method();
-  
-  ctx = SSL_CTX_new (meth);
-  
-  if (!ctx) {
-    ERR_print_errors_fp(stderr);
-    exit(2);
-  }
-  
-  if (SSL_CTX_use_certificate_file(ctx, RSA_CERTF, SSL_FILETYPE_PEM) <= 0) {
-    ERR_print_errors_fp(stderr);
-    exit(3);
-  }
-  if (SSL_CTX_use_PrivateKey_file(ctx, RSA_KEYF, SSL_FILETYPE_PEM) <= 0) {
-    ERR_print_errors_fp(stderr);
-    exit(4);
-  }
+	/* for performance measurements */
+	unsigned long long diff;
 
-  if (!SSL_CTX_check_private_key(ctx)) {
-    fprintf(stderr,"Private key does not match the certificate public key\n");
-    exit(5);
-  }
+	/* arguments */
+	unsigned int port;
 
-  if (SSL_CTX_use_n_certificate_file(DIVERSITY_FACTOR, ctx, ECDH_CERTF, SSL_FILETYPE_PEM) <= 0) {
-    ERR_print_errors_fp(stderr);
-    exit(3);
-  }
-  if (SSL_CTX_use_n_PrivateKey_file(DIVERSITY_FACTOR, ctx, ECDH_KEYF, SSL_FILETYPE_PEM) <= 0) {
-    ERR_print_errors_fp(stderr);
-    exit(4);
-  }
+	/* Arguments validation */
+	if (argc != 2) {
+		printf("Usage: ./server <port>\n");
+		exit(1);
+	}
+	port = atoi(argv[1]);
+	debug_printf("Arguments: Port %d\n", port);
 
-  if (!SSL_CTX_check_n_private_key(DIVERSITY_FACTOR, ctx)) {
-    fprintf(stderr,"Second private key does not match the certificate public key\n");
-    exit(5);
-  }
+	demo_banner();
 
-  /* ----------------------------------------------- */
-  /* Prepare TCP socket for receiving connections */
+	/* SSL preliminaries. We keep the certificate and key with the context. */
 
-  listen_sd = socket (AF_INET, SOCK_STREAM, 0);
-  CHK_ERR(listen_sd, "socket");
-  
-  memset(&sa_serv, 0, sizeof(sa_serv));
-  sa_serv.sin_family      = AF_INET;
-  sa_serv.sin_addr.s_addr = INADDR_ANY;
-  sa_serv.sin_port        = htons (port);          /* Server Port number */
-  
-  err = bind(listen_sd, (struct sockaddr*) &sa_serv, sizeof (sa_serv));
-  CHK_ERR(err, "bind");
-  debug_printf("Server bound to port %d\n", ntohs(sa_serv.sin_port));
+	SSL_load_error_strings();
+	OpenSSL_add_ssl_algorithms();
+	meth = TLSv1_2_method();
 
+	ctx = SSL_CTX_new(meth);
 
-  /* Receive a TCP connection. */
+	if (!ctx) {
+		ERR_print_errors_fp(stderr);
+		exit(2);
+	}
 
-  err = listen (listen_sd, 5);
-  CHK_ERR(err, "listen");
-  debug_printf("Server listening on port %d\n", ntohs(sa_serv.sin_port));
+	if (SSL_CTX_use_certificate_file(ctx, RSA_CERTF, SSL_FILETYPE_PEM) <= 0) {
+		ERR_print_errors_fp(stderr);
+		exit(3);
+	}
+	if (SSL_CTX_use_PrivateKey_file(ctx, RSA_KEYF, SSL_FILETYPE_PEM) <= 0) {
+		ERR_print_errors_fp(stderr);
+		exit(4);
+	}
 
-  
-  client_len = sizeof(sa_cli);
-  sd = accept (listen_sd, (struct sockaddr*) &sa_cli, &client_len);
-  CHK_ERR(sd, "accept");
-  debug_print("Server accepted connection\n");
+	if (!SSL_CTX_check_private_key(ctx)) {
+		fprintf(stderr,
+				"Private key does not match the certificate public key\n");
+		exit(5);
+	}
 
-  close (listen_sd);
-  debug_print("Server socket closed\n");
+	if (SSL_CTX_use_n_certificate_file(DIVERSITY_FACTOR, ctx, ECDH_CERTF,
+	SSL_FILETYPE_PEM) <= 0) {
+		ERR_print_errors_fp(stderr);
+		exit(3);
+	}
+	if (SSL_CTX_use_n_PrivateKey_file(DIVERSITY_FACTOR, ctx, ECDH_KEYF,
+	SSL_FILETYPE_PEM) <= 0) {
+		ERR_print_errors_fp(stderr);
+		exit(4);
+	}
 
-  // convert IP address to string
-  char addr_buf[INET_ADDRSTRLEN];
-  inet_ntop(AF_INET, &(sa_cli.sin_addr.s_addr), addr_buf, INET_ADDRSTRLEN);
-  // print IP and port
-  debug_printf("Connection from %s, port %d\n",
-		  addr_buf, ntohs(sa_cli.sin_port));
+	if (!SSL_CTX_check_n_private_key(DIVERSITY_FACTOR, ctx)) {
+		fprintf(stderr,
+				"Second private key does not match the certificate public key\n");
+		exit(5);
+	}
 
-  
-  /* ----------------------------------------------- */
-  /* TCP connection is ready. Do server side SSL. */
+	/* ----------------------------------------------- */
+	/* Prepare TCP socket for receiving connections */
 
-  ssl = SSL_new (ctx);
-  CHK_NULL(ssl);     /* CHECKED */
-  SSL_set_fd (ssl, sd);
-  err = SSL_accept (ssl);
-  CHK_SSL(err);      /* CHECKED */
-  
-  /* Get the cipher - opt */
-  
-  debug_printf("SSL connection using %s\n", SSL_get_cipher (ssl));
-  debug_printf("SSL connection using %s\n", SSL_get_n_cipher (DIVERSITY_FACTOR, ssl));
-  
-  /* Get client's certificate (note: beware of dynamic allocation) - opt */
+	listen_sd = socket(AF_INET, SOCK_STREAM, 0);
+	CHK_ERR(listen_sd, "socket");
 
-  /*
-  client_cert = SSL_get_peer_certificate (ssl);
-  if (client_cert != NULL) {
-    
-    printf ("Client certificate:\n");
-    
-    str = X509_NAME_oneline (X509_get_subject_name (client_cert), 0, 0);
-    CHK_NULL(str);
-    printf ("\t subject: %s\n", str);
-    OPENSSL_free (str);
-    
-    str = X509_NAME_oneline (X509_get_issuer_name  (client_cert), 0, 0);
-    CHK_NULL(str);
-    printf ("\t issuer: %s\n", str);
-    OPENSSL_free (str);    
-    X509_free (client_cert);
-    
-  } else
-    printf ("Client does not have certificate.\n");
-  
-  */
+	memset(&sa_serv, 0, sizeof(sa_serv));
+	sa_serv.sin_family = AF_INET;
+	sa_serv.sin_addr.s_addr = INADDR_ANY;
+	sa_serv.sin_port = htons(port); /* Server Port number */
 
-  /* DATA EXCHANGE - Receive message and send reply. */
-    
-//  err = SSL_read (ssl, buf, sizeof(buf) - 1);
-//  CHK_SSL(err);
-//  buf[err] = '\0';
-//  debug_printf("Got %d chars\n", err);
-//  debug_printf("Received file name '%s'\n", buf);
+	err = bind(listen_sd, (struct sockaddr*) &sa_serv, sizeof(sa_serv));
+	CHK_ERR(err, "bind");
+	debug_printf("Server bound to port %d\n", ntohs(sa_serv.sin_port));
 
-  // read file name
-  err = readSSLLine(ssl, buf, sizeof(buf));
-  CHK_SSL(err);
-  char file_name[256];
-  strncpy(file_name, buf, err-1);
-  debug_printf("Requested file name '%s'\n", file_name);
+	/* Receive a TCP connection. */
 
-  // read empty line separator
-  err = readSSLLine(ssl, buf, sizeof(buf));
-  CHK_SSL(err);
-  debug_print("Read separator line\n");
+	err = listen(listen_sd, 5);
+	CHK_ERR(err, "listen");
+	debug_printf("Server listening on port %d\n", ntohs(sa_serv.sin_port));
 
+	client_len = sizeof(sa_cli);
+	sd = accept(listen_sd, (struct sockaddr*) &sa_cli, &client_len);
+	CHK_ERR(sd, "accept");
+	debug_print("Server accepted connection\n");
 
-  FILE *file;
-  //char *buffer;
-  long file_len;
+	close(listen_sd);
+	debug_print("Server socket closed\n");
 
-  file = fopen(file_name, "rb");	// Open the file in binary mode
-  fseek(file, 0, SEEK_END);	// Jump to the end of the file
-  file_len = ftell(file);	// Get the current byte offset in the file
-  rewind(file);				// Jump back to the beginning of the file
-  debug_printf("File size is %ld\n", file_len);
+	// convert IP address to string
+	inet_ntop(AF_INET, &(sa_cli.sin_addr.s_addr), addr_buf, INET_ADDRSTRLEN);
+	// print IP and port
+	debug_printf("Connection from %s, port %d\n", addr_buf,
+			ntohs(sa_cli.sin_port));
 
-  char sizestring[205];
-  sprintf(sizestring, "%ld", file_len);
-  err = SSL_write(ssl, sizestring, strlen(sizestring));
-  err = SSL_write(ssl, "\n", sizeof(char));
-  debug_print("Wrote file size\n");
+	/* ----------------------------------------------- */
+	/* TCP connection is ready. Do server side SSL. */
 
-  err = SSL_write(ssl, "\n", sizeof(char));
-  debug_print("Wrote separator line\n");
+	ssl = SSL_new(ctx);
+	CHK_NULL(ssl); /* CHECKED */
+	SSL_set_fd(ssl, sd);
+	err = SSL_accept(ssl);
+	CHK_SSL(err); /* CHECKED */
 
+	/* Get the cipher - opt */
 
+	debug_printf("SSL connection using %s\n", SSL_get_cipher (ssl));
+	debug_printf("SSL connection using %s\n",
+			SSL_get_n_cipher (DIVERSITY_FACTOR, ssl));
 
-  int bytesRead = 0;
-  int workBufferSize = 1024;
-  char workBuffer[1024];
+	/* Get client's certificate (note: beware of dynamic allocation) - opt */
 
-  // read until all bytes are received
-  while(bytesRead < file_len) {
-      int result = fread(workBuffer, sizeof(char), workBufferSize, file);
-      debug_printf("Read %d bytes from file\n", result);
-      if(result == -1) {
-          debug_println("Error while reading data from file!");
-          return -1;
-      }
-      bytesRead += result;
+	client_cert = SSL_get_peer_certificate(ssl);
+	if (client_cert != NULL) {
+		debug_println("Client certificate:");
 
-      err = SSL_write(ssl, workBuffer, result);
-      debug_printf("Wrote %d bytes to secure socket\n", err);
-  }
-  debug_println("Read all expected bytes");
-  fclose(file);                                   	  // Close the file
+		str = X509_NAME_oneline(X509_get_subject_name(client_cert), 0, 0);
+		CHK_NULL(str);
+		debug_printf("\t subject: %s\n", str);
+		OPENSSL_free(str);
 
+		str = X509_NAME_oneline(X509_get_issuer_name(client_cert), 0, 0);
+		CHK_NULL(str);
+		debug_printf("\t issuer: %s\n", str);
+		OPENSSL_free(str);
+		X509_free(client_cert);
 
-//  buffer = (char *)malloc((file_len+1)*sizeof(char)); // Enough memory for file + \0
-//  fread(buffer, file_len, 1, file);                	  // Read in the entire file
-//  buffer[file_len] = '\0';
-//  fclose(file);                                   	  // Close the file
-      
-//  char filelen[512];
-//  sprintf(filelen, "%ld", file_len);
-    
-//  err = SSL_write(ssl, filelen, strlen(filelen));
- 
-  /*int counter = 0;
-  
-//  for (counter = 0; counter < 50; counter++){*/
-//  /*********/
-//    err = 0;
-//    int i = file_len;
-//    timeval start, end;
-//
-//    gettimeofday(&start, NULL);
-//
-//    for(i = file_len; i - MAX_MSG_SIZE > 0; i -= MAX_MSG_SIZE){
-//      err += SSL_write (ssl, buffer+err, MAX_MSG_SIZE);
-//    }
-//
-//    err += SSL_write (ssl, buffer+err, i);
-//
-//    gettimeofday(&end, NULL);
-//    diff = 1000 * (end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec) / 1000;
-//    printf ("%llu\n", diff);
-//    printf ("The vtTLS took %llu ms to send %s.\n", diff, buf);
-//    diff = 0;
-//
- /*********/
-  //}
-  
-  // printf("-- total_size: %d\n", err);
-    
-  /* Clean up. */
+	} else {
+		debug_println("Client does not have certificate.");
+	}
 
-  //free(buffer);
-  
-  close (sd);
-  debug_print("Socket closed\n");
+	/* --------------------------------------------------- */
+	/* DATA EXCHANGE - Receive message and send reply. */
 
-  SSL_free (ssl);
-  SSL_CTX_free (ctx);
-  debug_print("SSL terminated\n");
-  
-  return 0;
+	/* REQUEST */
+
+	// read file name line
+	err = readSSLLine(ssl, buf, BUF_SZ);
+	CHK_SSL(err);
+	strncpy(file_name, buf, err - 1);
+	debug_printf("Requested file name '%s'\n", file_name);
+
+	// read empty line separator
+	err = readSSLLine(ssl, buf, BUF_SZ);
+	CHK_SSL(err);
+
+	// open file to read
+	FILE *file;
+	long file_len;
+
+	file = fopen(file_name, "rb");	// Open the file in binary mode
+	if (file == NULL) {
+		debug_printf("fopen() error: %s\n", strerror(errno));
+		// -1 means that the file does not exist
+		file_len = -1;
+	} else {
+		debug_println("File open");
+		fseek(file, 0, SEEK_END);	// Jump to the end of the file
+		file_len = ftell(file);		// Get the current byte offset in the file
+		rewind(file);				// Jump back to the beginning of the file
+		debug_printf("File size is %ld\n", file_len);
+	}
+
+	/* RESPONSE */
+
+	// write file size (as a string)
+	sprintf(file_len_str, "%ld", file_len);
+	err = SSL_write(ssl, file_len_str, strlen(file_len_str));
+	// add end-of-line
+	err = SSL_write(ssl, "\n", sizeof(char));
+	debug_println("Wrote file size");
+
+	// write empty line separator
+	err = SSL_write(ssl, "\n", sizeof(char));
+	debug_println("Wrote separator line");
+
+	if (file_len > 0) {
+		// write file to secure socket
+		int bytesRead = 0;
+		while (bytesRead < file_len) {
+			int readResult = fread(buf, sizeof(char), BUF_SZ, file);
+			CHK_ERR(readResult, "file");
+			debug_printf("Read %d bytes from file\n", readResult);
+			bytesRead += readResult;
+
+			int writeResult = SSL_write(ssl, buf, readResult);
+			CHK_SSL(writeResult);
+			debug_printf("Wrote %d bytes to secure socket\n", writeResult);
+		}
+		debug_println("Wrote all file bytes");
+		// Close the file
+		fclose(file);
+		debug_println("File closed");
+	}
+
+	/* Clean up. */
+	close(sd);
+	debug_println("Client socket closed");
+
+	SSL_free(ssl);
+	SSL_CTX_free(ctx);
+
+	return 0;
 }
 /* EOF - server.cpp */
